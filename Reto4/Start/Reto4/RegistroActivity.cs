9ea777b804f39e03bc7235aa8baffca2089dac1f
Ramses Abdiel Santos Beltran <ranabdiel@hotmail.com>
2017-04-05 09:56:54 -0600

    Se suben los cambios para completar el reto 4 del xamarin championship

--- Reto4/Start/Reto4/RegistroActivity.cs
+++ Reto4/Start/Reto4/RegistroActivity.cs
@@ -15,12 +15,14 @@ namespace Reto4
     [Activity(Label = "Registrar datos")]
     public class RegistroActivity : Activity
     {
+        TextView txtView;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Registro);
             FindViewById<Button>(Resource.Id.btnConsulta).Click += OnBtnConsultaClick;
+            txtView = FindViewById<TextView>(Resource.Id.textView);
         }
     
         async void OnBtnConsultaClick(object sender, EventArgs e)
@@ -31,6 +33,15 @@ namespace Reto4
                 // Retrieve the values the user entered into the UI
                 string AndroidId = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
 
+                string email = FindViewById<EditText>(Resource.Id.editTextEmail).Text;
+                await serviceHelper.BuscarRegistros(email);
+
+                foreach (var item in serviceHelper.items)
+                {
+                    txtView.Text += String.Format("{0} {1} {2} \n", item.DeviceId, item.Email, item.Reto);
+                }
+
+                await serviceHelper.InsertarEntidad(email, "reto4+" + serviceHelper.items.Count, AndroidId);
 
                 SetResult(Result.Ok, Intent);
 
--- Reto4/Start/Reto4/Services/ServiceHelper.cs
+++ Reto4/Start/Reto4/Services/ServiceHelper.cs
@@ -8,5 +8,25 @@ namespace Reto4.Services
         MobileServiceClient clienteServicio = new MobileServiceClient(@"http://xamarinchampions.azurewebsites.net");
 
         private IMobileServiceTable<TorneoItem> _TorneoItemTable;
+
+        public System.Collections.Generic.List<TorneoItem> items;
+
+        public async Task BuscarRegistros(string correo)
+        {
+            _TorneoItemTable = clienteServicio.GetTable<TorneoItem>();
+            items = await _TorneoItemTable.Where(torneoItem => torneoItem.Email == correo).ToListAsync();
+        }
+
+        public async Task InsertarEntidad(string direccionCorreo, string reto, string AndroidId)
+        {
+            _TorneoItemTable = clienteServicio.GetTable<TorneoItem>();
+
+            await _TorneoItemTable.InsertAsync(new TorneoItem
+            {
+                Email = direccionCorreo,
+                Reto = reto,
+                DeviceId = AndroidId
+            });
+        }
     }
 }
