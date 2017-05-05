0d90718e84b1d50ba563bca28668ab7c371e4455
Ramses Abdiel Santos Beltran <ranabdiel@hotmail.com>
2017-05-04 22:20:32 -0600

    Se realiza el reto 6 del championship. Se trata de tomar una foto y de analizar las emociones de la cara.

---
+++ Reto6/Start/Emotions/ServiceEmotions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ProjectOxford.Emotion;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emotions
+{
+    public class ServiceEmotions
+    {
+        static string key = "9c89f775c8114df7b0631cd487746503";
+
+        public static async Task<Dictionary<string, float>> GetEmotions(Stream stream)
+        {
+            EmotionServiceClient cliente = new EmotionServiceClient(key);
+            var emotions = await cliente.RecognizeAsync(stream);
+
+            if (emotions == null || emotions.Count() == 0) return null;
+
+            return emotions[0].Scores.ToRankedList().ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
---
+++ Reto6/Start/Emotions/ServiceImage.cs
@@ -0,0 +1,24 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emotions
+{
+    public class ServiceImage
+    {
+        public static async Task<MediaFile> TakePicture(bool useCam = true)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (useCam) { if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported) { return null; } }
+
+            var file = useCam ? await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions { Directory = "Championship", Name = "Reto6_Test.jpg" }) : await CrossMedia.Current.PickPhotoAsync();
+
+            return file;
+        }
+    }
+}
